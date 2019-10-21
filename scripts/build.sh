7a08e952f94a39680781e714b2dfc09c84c90541
Adrian Gschwend <ktk@netlabs.org>
2019-10-21 11:59:51 +0200

    Convert every file in input

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-java -jar lib/rmlmapper-4.0.0.jar -v -m config/cam53.ttl -o target/complete.nt
+#!/bin/bash
+
+for file in ./input/*.xml
+do
+  filename=$(basename "$file")
+
+  if [ ! -f "target/"$filename".nt" ]
+  then
+    echo Processing $filename...
+    java -jar lib/cli-0.0.9-SNAPSHOT-jar-with-dependencies.jar -i "input/"$filename -m config/cam53.ttl -of nt -o "target/"$filename".nt"
+
+  else
+    echo Found "target/"$filename".nt", skipping $file
+  fi
+done;
